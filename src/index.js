047f2c9c4ec439870b94a2f63291c4ba2a637e20
Inda Smith <inda.k.smith@gmail.com>
2024-05-02 21:56:23 -0400

    I'm too tired to keep going. I will pick this up later.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
+// import React from "react";
 import "./index.css";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes";
 
+const BrowserRouter = createBrowserRouter({routeConfig: routes});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+
+const renderApp = () => (
+    <BrowserRouter>
+    <RouterProvider>
+        {routes.map((route,index) => (
+            <route.component key = {index}/>
+        ))}
+    </RouterProvider>
+    </BrowserRouter>
+)
+
+root.render(renderApp());
+
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,14 @@
-
+// import React from "react";
+import Home from "./pages/Home.js"
+import Directors from "./pages/Directors.js"
+import Actors from "./pages/Actors.js"
+import Movie from "./pages/Movie.js"
 
 const routes = [
-   
+  {path: '/', component: Home, exact: true},
+  {path: "/actors", component: Actors},
+  {path: "/directors", component: Directors},
+  {path: "/movie:id", component: Movie}
   ];
 
 export default routes;
